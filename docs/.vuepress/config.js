9ac8f2f9baa85b4124299b77689b2731e9bda708
SuperMaryer <1012073997@qq.com>
2022-08-08 23:43:01 +0800

    feat: sideBar config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,47 +1,20 @@
+const navConfig =  require("./navConfig");
+const sidebarConfig =  require("./sidebarConfig");
 module.exports = {
     title: "daily-record",
-    description: "来自一个前端程序猿的记录",
-    base: "/daily-record/",
-    themeConfig: {
-        siteTitle: "daily-record",
+    description: "来自一个前端程序猿的日常记录",
+    base: "/daily-record/", // github
+    // base: '/',  //阿里云服务器
+    // dest: './dist',   // 设置输出目录
+    // port: 2333, //端口
+    themeConfig: { //主题配置
+        // siteTitle: "daily-record",
         logo: "/assets/images/logo.png",
-        nav: [
-            { text: "Home", link: "/" },
-            { text: "Github", link: "https://github.com/supermaryer/daily-record" },
-            { text: "VuePress", link: "https://vuepress.vuejs.org/" },
-            // 下拉
-            // {
-            //     text: "Drop Menu",
-            //     items: [
-            //       { text: 'Item A', link: '/item-1' },
-            //       { text: 'Item B', link: '/item-2' },
-            //       { text: 'Item C', link: '/item-3' }
-            //     ]
-            // }
-            // {
-            //     text: "Drop Menu",
-            //     items: [
-            //       {
-            //         items: [
-            //           { text: "Item A1", link: "/item-A1" },
-            //           { text: "Item A2", link: "/item-A2" },
-            //         ],
-            //       },
-            //       {
-            //         items: [
-            //           { text: "Item B1", link: "/item-B1" },
-            //           { text: "Item B2", link: "/item-B2" },
-            //         ],
-            //       },
-            //     ],
-            // },
-        ],
-        sidebar: [
-            // "/", 
-            // "/componentDocs/card",
-            // "/componentDocs/time-period-list",
-            // "/componentDocs/content-section",
-        ],
+        nav: navConfig, // 导航栏配置
+        sidebar: sidebarConfig, // 侧边栏配置
+        sidebarDepth: 2,
+        displayAllHeaders: true,
+        activeHeaderLinks: false,
     },
     plugins: [
         // require('./my-plugin.js'),
---
+++ docs/.vuepress/navConfig.js
@@ -0,0 +1,34 @@
+
+const nav = [
+    { text: "Home", link: "/" },
+    { text: "Github", link: "https://github.com/supermaryer/daily-record" },
+    { text: "VuePress", link: "https://vuepress.vuejs.org/" },
+    // 下拉
+    // {
+    //     text: "Drop Menu",
+    //     items: [
+    //       { text: 'Item A', link: '/item-1' },
+    //       { text: 'Item B', link: '/item-2' },
+    //       { text: 'Item C', link: '/item-3' }
+    //     ]
+    // }
+    // {
+    //     text: "Drop Menu",
+    //     items: [
+    //       {
+    //         items: [
+    //           { text: "Item A1", link: "/item-A1" },
+    //           { text: "Item A2", link: "/item-A2" },
+    //         ],
+    //       },
+    //       {
+    //         items: [
+    //           { text: "Item B1", link: "/item-B1" },
+    //           { text: "Item B2", link: "/item-B2" },
+    //         ],
+    //       },
+    //     ],
+    // },
+]
+
+module.exports = nav;
